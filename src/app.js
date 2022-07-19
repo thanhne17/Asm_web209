befe1f1ef3a8412410b9f3237cc3f6a92462b2e6
sangluong2k2 <luongduysang2k2@gmail.com>
2022-07-19 20:34:29 +0700

    asdasdasda

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-s
+import express from "express"
+import mongoose from "mongoose"
+
+
+import cors from 'cors'
+
+const app = express();
+
+app.use(express.json())
+app.use(cors());
+
+mongoose.connect("mongodb://localhost:27017/web209")
+.then(() => console.log("connect db thanh cong"))
+.catch((error) => console.log(error))
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log(`server running port ${PORT}`)
+})
