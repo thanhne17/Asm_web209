9dadaf98782f80282127dbb8cc7891b805a86974
sangluong2k2 <luongduysang2k2@gmail.com>
2022-07-20 20:57:22 +0700

    multi layouts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AppPropsWithLayout } from '../models/layout'
+import LayoutAdmin from '../components/layouts/admin';
+import Layout from '../components/layouts';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const LayoutWrapper = Component.Layout ?? Layout ;
+  return  (<LayoutWrapper><Component {...pageProps} /></LayoutWrapper>)
 }
 
 export default MyApp
---
+++ pages/admin.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LayoutAdmin from '../components/layouts/admin'
+
+type Props = {}
+
+const AdminPage = (props: Props) => {
+  return (
+    <div>AdminPage</div>
+  )
+}
+AdminPage.Layout = LayoutAdmin
+
+export default AdminPage
