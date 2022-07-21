c1259f98147ed1a1876e12fc2288ff809c65e0f8
sangluong2k2 <luongduysang2k2@gmail.com>
2022-07-21 13:44:19 +0700

    multi layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AppPropsWithLayout } from '../model/layout'
+import SiteLayout from '../components/layout/SiteLayout'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const LayoutWrapper = Component.Layout ?? SiteLayout
+  return (<LayoutWrapper><Component {...pageProps} />
+</LayoutWrapper>)}
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,9 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <SiteLayout>
       <div className="h-[1000px]">
         <h1 className='text-xl text-[#ccc]'>hi</h1>
       </div>
-    </SiteLayout>
   )
 }
 
